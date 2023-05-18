9692e1b09d72300bdfd59152fb92b08230849f4c
junnjuns <donamjun1@naver.com>
2023-05-18 17:41:53 +0900

    feat: @RequestParam 추가하여 파라미터 사용 개선

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -5,8 +5,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -17,10 +20,74 @@ public class RequestParamController {
         String username = request.getParameter("username");
         int age = Integer.parseInt(request.getParameter("age"));
 
-        log.info("username={}, age={}", username,age);
+        log.info("username={}, age={}", username, age);
 
         response.getWriter().write("ok");
     }
 
+    @ResponseBody   // @RestController가 아닐 때 리턴 값 문자로 반환해준다.
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge) {
 
+        log.info("username={}, age={}", memberName, memberAge);
+
+        return "ok";
+
+    }
+
+
+    @ResponseBody   // @RestController가 아닐 때 리턴 값 문자로 반환해준다.
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,  // 파라미터명 == 변수명 이면 RequestParam 다음 () 생략가능
+            @RequestParam int age) {
+
+        log.info("username={}, age={}", username, age);
+
+        return "ok";
+    }
+
+    @ResponseBody   // @RestController가 아닐 때 리턴 값 문자로 반환해준다.
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) { // String, int, Integer 등 단순 타입일 때 @RequestParam 생략 가능
+
+        log.info("username={}, age={}", username, age);
+
+        return "ok";
+    }
+
+
+    @ResponseBody   // @RestController가 아닐 때 리턴 값 문자로 반환해준다.
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username, // requried = true 이면 반드시 데이터가 들어와야 함, 기본값 true
+            @RequestParam(required = false) Integer age) { // int는 null 이 들어갈 수 없다. Integer는 객체형이기 때문에 null이 들어올 수 있음
+
+        log.info("username={}, age={}", username, age);
+
+        return "ok";
+    }
+
+    @ResponseBody   // @RestController가 아닐 때 리턴 값 문자로 반환해준다.
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(defaultValue = "guest") String username, //값이 안 들어올 때 defaultValue로 값을 설정할 수 있음
+            @RequestParam(defaultValue = "-1") Integer age){
+
+        log.info("username={}, age={}", username, age);
+
+        return "ok";
+    }
+
+
+    @ResponseBody   // @RestController가 아닐 때 리턴 값 문자로 반환해준다.
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap){
+
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+
+        return "ok";
+    }
 }
