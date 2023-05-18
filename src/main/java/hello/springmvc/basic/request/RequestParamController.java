9399d36d4b30885defbd599bfcf3efd02d9cea17
junnjuns <donamjun1@naver.com>
2023-05-18 17:59:10 +0900

    feat: @ModelAttribute 추가하여 객체 생성 자동화

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,9 +1,11 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -88,6 +90,25 @@ public class RequestParamController {
 
         log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
 
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    //해당 프로퍼티의 setter를 호출해서 파라미터의 값을 입력한다.
+    public String modelAttributeV1(@ModelAttribute HelloData helloData){
+
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    //ModelAttribute 생략 가능
+    public String modelAttributeV2(HelloData helloData){
+
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
         return "ok";
     }
 }
